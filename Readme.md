69ea4b8a76d5e615ce700afe95fa426c15a758ac
Shalltearz <42479034+Tera-Shiraneko@users.noreply.github.com>
2019-01-25 02:11:35 +0100

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,25 @@
+## Tera proxy module to test abnormalities clientsided.
+
+---
+
+## Console Command
+- Type `/8 abnormalitytester` to enable or disable the module default is enabled.
+- Type `/8 abstart + id` to start an abnormality an list of ids can be found in abnormalinfo section.
+- Type `/8 abduration + ms` to configure the abnormality duration in milliseconds default is ten minutes.
+- Type `/8 abend + id` to end the triggered abnormality should be the same as the one you started before.
+- Type `/8 abstack + stacks` to configure the amount of stacks you want to be shown default is one stack.
+
+---
+
+## Interface Command
+- Type `/8 abnormalitytester` to enable or disable the module and set up stacks and duration.
+
+---
+
+## Abnormalinfo
+- [Abnormality Overview](https://github.com/neowutran/TeraDpsMeterData/tree/master/hotdot)
+
+---
+
+## Note
+- The commands should be written without the plus just an space between it.
---
+++ index.js
@@ -0,0 +1,93 @@
+const SettingsUI = require('tera-mod-ui').Settings;
+
+module.exports = function Abnormalitytester(mod) {
+
+    if (mod.proxyAuthor !== 'caali' || !global.TeraProxy)
+        mod.warn('You are trying to use this module on an unsupported version of tera-proxy. It may not work as expected, and even if it does now it may break at any point in the future.');
+
+    let ids;
+
+    mod.command.add('abnormalitytester', () => {
+        if (ui) {
+            ui.show();
+        } else {
+            mod.settings.enabled = !mod.settings.enabled;
+            mod.command.message(`Abnormality tester is now ${mod.settings.enabled ? "enabled" : "disabled"}.`);
+        }
+    });
+
+    mod.command.add('abstart', (id) => {
+        ids = Number.parseInt((id.replace(/\D+/g, '')), 10);
+        abbegin(ids);
+        mod.command.message('Attempted to start abnormality ' + ids + '.');
+        if (isNaN(ids)) {
+            mod.command.message('Please enter an valid abnormality id.');
+        }
+        if (!mod.settings.enabled) {
+            mod.command.message('Please activate the module first.');
+        }
+    });
+
+    mod.command.add('abend', (id) => {
+        ids = Number.parseInt((id.replace(/\D+/g, '')), 10);
+        abend(ids);
+        mod.command.message('Attempted to end abnormality ' + ids + '.');
+        if (isNaN(ids)) {
+            mod.command.message('Please enter an valid abnormality id.');
+        }
+        if (!mod.settings.enabled) {
+            mod.command.message('Please activate the module first.');
+        }
+    });
+
+    mod.command.add('abduration', (id) => {
+        mod.settings.duration = Number.parseInt((id.replace(/\D+/g, '')), 10);
+        mod.command.message('Abnormality duration set to ' + mod.settings.duration + '.');
+        if (isNaN(mod.settings.duration)) {
+            mod.command.message('Please enter an valid duration number.');
+        }
+    });
+
+    mod.command.add('abstack', (id) => {
+        mod.settings.stack = Number.parseInt((id.replace(/\D+/g, '')), 10);
+        mod.command.message('Abnormality stacks set to ' + mod.settings.stack + '.');
+        if (isNaN(mod.settings.stack)) {
+            mod.command.message('Please enter an valid stack number.');
+        }
+    });
+
+    function abbegin(iden) {
+        if (!mod.settings.enabled) return;
+        mod.send('S_ABNORMALITY_BEGIN', 3, {
+            target: mod.game.me.gameId,
+            source: mod.game.me.gameId,
+            id: iden,
+            duration: mod.settings.duration,
+            unk: 0,
+            stacks: mod.settings.stack,
+            unk2: 0,
+            unk3: 0
+        });
+    }
+
+    function abend(iden) {
+        if (!mod.settings.enabled) return;
+        mod.send('S_ABNORMALITY_END', 1, {
+            target: mod.game.me.gameId,
+            id: iden
+        });
+    }
+
+    let ui = null;
+    if (global.TeraProxy.GUIMode) {
+        ui = new SettingsUI(mod, require('./settings_structure'), mod.settings, { height: 190 }, { alwaysOnTop: true });
+        ui.on('update', settings => { mod.settings = settings; });
+
+        this.destructor = () => {
+            if (ui) {
+                ui.close();
+                ui = null;
+            }
+        };
+    }
+};
